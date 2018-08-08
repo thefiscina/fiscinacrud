8d2a02eca26e7be0263c82461325ab9b98dfe714
nusa-note\nusa <jonathas@rastrocompleto.com.br>
2018-08-07 23:28:56 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { HeroSliderComponent } from './hero-slider/hero-slider.component';
+import { BoxesComponent } from './boxes/boxes.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeEventsComponent } from './home-events/home-events.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SiteHeaderComponent,
+    HeroSliderComponent,
+    BoxesComponent,
+    WelcomeComponent,
+    HomeEventsComponent,
+    ProjetosComponent,
+    TimeComponent      
   ],
   imports: [
-    BrowserModule
+    BrowserModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
