254cbcad8fffb14ae3fac8e08b270904f1a8cec7
nusa-note\nusa <jonathas@rastrocompleto.com.br>
2018-08-22 00:35:34 -0300

    time

--- src/app/time/time.component.ts
+++ src/app/time/time.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface,
+  SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
 @Component({
   selector: 'app-time',
   templateUrl: './time.component.html',
   styleUrls: ['./time.component.css']
 })
 export class TimeComponent implements OnInit {
-
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    speed:2000,    
+    loop:true    
+  };
   constructor() { }
 
   ngOnInit() {
