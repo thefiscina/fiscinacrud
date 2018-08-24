198ba353339c45c1589dc136ea6dd392a4a32870
nusa-note\nusa <jonathas@rastrocompleto.com.br>
2018-08-24 01:10:57 -0300

    mobile menu

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,69 +2,14 @@ $(document).ready(function () {
      'use strict';
 
     // Main Navigation
-    $('.hamburger-menu').on('click', function () {
-        $(this).toggleClass('open');
-        $('.site-navigation').toggleClass('show');
-    });
+    // $('.hamburger-menu').on('click', function () {
+    //     $(this).toggleClass('open');
+    //     $('.site-navigation').toggleClass('show');
+    // });
     
-    // Accordion & Toggle
-    $('.accordion-wrap.type-accordion').collapsible({
-        accordion: true,
-        contentOpen: 0,
-        arrowRclass: 'arrow-r',
-        arrowDclass: 'arrow-d'
-    });
-
-    $('.accordion-wrap .entry-title').on('click', function () {
-        $('.accordion-wrap .entry-title').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    // Tabs
-    $(function () {
-        $('.tab-content:first-child').show();
-
-        $('.tab-nav').bind('click', function (e) {
-            $this = $(this);
-            $tabs = $this.parent().parent().next();
-            $target = $($this.data("target"));
-            $this.siblings().removeClass('active');
-            $target.siblings().css("display", "none");
-            $this.addClass('active');
-            $target.fadeIn("slow");
-        });
-
-        $('.tab-nav:first-child').trigger('click');
-    });
-
   
-    // Counter
-    $(".start-counter").each(function () {
-        var counter = $(this);
-
-        counter.countTo({
-            formatter: function (value, options) {
-                return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-            }
-        });
-    });
 
     // Bar Filler
     $('.porcentagem').barfiller({ barColor: '#a8518a', duration: 1500 });
-
-
-    // Load more
-    let $container = $('.portfolio-container');
-    let $item = $('.portfolio-item');
-
-    $item.slice(0, 9).addClass('visible');
-
-    $('.load-more-btn').on('click', function (e) {
-        e.preventDefault();
-
-        $('.portfolio-item:hidden').slice(0, 9).addClass('visible');
-    });
-
- 
 
 });
